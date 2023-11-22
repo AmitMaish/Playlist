78cce08dc4a3cd5b7bb94615098af3c9627a9af7
AmitMaish <83043520+AmitMaish@users.noreply.github.com>
2023-11-21 17:02:25 -0800

    Update __main__.py
    Converted file playback to be asynchronous with callbacks.

--- __main__.py
+++ __main__.py
@@ -1,5 +1,6 @@
 import wave
 import sys
+import os
 import math
 import time
 
@@ -7,34 +8,45 @@ import pyaudio
 
 from rich.progress import Progress
 
-CHUNK = 1024
+# import keyboard
+
+BufferSize = 1024
 
 if len(sys.argv) < 2:
 	print(f'Plays a wave file. Usage: {sys.argv[0]} filename.wav')
 	sys.exit(-1)
 
+# os.system('clear')
+
 with wave.open(sys.argv[1], 'rb') as wf:
-	# Instantiate PyAudio and initialize PortAudio system resources (1)
-	p = pyaudio.PyAudio()
-
-	# Open stream (2)
-	stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
-					channels=wf.getnchannels(),
-					rate=wf.getframerate(),
-					output=True)
-	
-	# Make a progress bar for the track
-	with Progress() as progress:
+	# Define callback for playback (1)
+	with Progress(auto_refresh=True) as progress:
 		task1 = progress.add_task('[red]' + sys.argv[1], total=wf.getnframes())
+		def callback(in_data, frame_count, time_info, status):
+			data = wf.readframes(frame_count)
+			progress.update(task1, advance=frame_count)
+			# If len(data) is less than requested frame_count, PyAudio automatically
+			# assumes the stream is finished, and the stream stops.
+			return (data, pyaudio.paContinue)
+		# keyboard.on_press_key("p", pause)
 
-		# Play samples from the wave file (3)
-		while len(data := wf.readframes(CHUNK)):  # Requires Python 3.8+ for :=
-			stream.write(data)
-			progress.update(task1, advance=CHUNK)
+   		# Instantiate PyAudio and initialize PortAudio system resources (2)
+		p = pyaudio.PyAudio()
 
+			# Open stream using callback (3)
+		stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
+						channels=wf.getnchannels(),
+						rate=wf.getframerate(),
+						output=True,
+						frames_per_buffer=BufferSize,
+						stream_callback=callback)
 
-	# Close stream (4)
-	stream.close()
+		# Wait for stream to finish (4)
+		while stream.is_active():
+			time.sleep(0.1)
 
-	# Release PortAudio system resources (5)
-	p.terminate()
+		# Close the stream (5)
+		stream.close()
+
+		# Release PortAudio system resources (6)
+		p.terminate()
